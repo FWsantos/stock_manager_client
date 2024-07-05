2de6e461f7c699e40c34bf806b3b648f2b1c05c1
FWsantos <fwsantos23@gmail.com>
2024-07-05 11:27:53 -0300

    show images

--- src/components/TableList.tsx
+++ src/components/TableList.tsx
@@ -3,54 +3,45 @@ import { Product } from "../store/interfaces";
 import axios from "axios";
 
 function TableList() {
-  const [data, setData] = useState<Product[]>([]);
+  const [produtos, setProdutos] = useState<Product[]>([]);
   const apiUrl = import.meta.env.VITE_API_URL;
 
   const fetchData = async () => {
-    axios.get(apiUrl + "/products").then((response) => {
-      setData(response.data);
-    });
+    const fetchProdutos = async () => {
+      try {
+        axios.get(apiUrl + "/produtos").then((response) => {
+          const data = response.data;
+          setProdutos(data);
+        });
+      } catch (error) {
+        console.error("Erro ao buscar produtos:", error);
+      } finally {
+        console.log("Busca de produtos finalizada.");
+      }
+    };
+
+    fetchProdutos();
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
+  const list = produtos.map((produto) => (
+    <li key={produto.id}>
+      <p>
+        {produto.name} {produto.price}
+      </p>
+      {produto.image && (
+        <img
+          src={`data:image/jpeg;base64,${produto.image}`}
+          alt={produto.name}
+        />
+      )}
+    </li>
+  ));
 
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </table>
-  );
+  return <ul>{list}</ul>;
 }
 
 export default TableList;
